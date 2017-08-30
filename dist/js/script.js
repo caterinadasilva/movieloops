41c7667073fa268365faec84e17eb59bd191d221
Syrimande <caterinadasilva@gmail.com>
2017-08-30 04:22:34 -0300

    pantalla de inicio y reestructuración

--- dist/js/script.js
+++ dist/js/script.js
@@ -12641,6 +12641,7 @@ $(document).ready(function(){
 	  $('[data-toggle="tooltip"]').tooltip()
 	});
 });
+if ($( "#monto-final" )) {
 	var countChecked = function() {
 	  var n = $( "input:checked" ).length;
 	  $( "#monto-final" ).text( n * 4300 );
@@ -12648,6 +12649,8 @@ $(document).ready(function(){
 	countChecked();
 	 
 	$( "input[type=checkbox]" ).on( "click", countChecked );
+}
+	
 
 
 $( document ).ready(function() {
