794072f94f2e73c9b38ba3432ec3890932fcefe5
Syrimande <caterinadasilva@gmail.com>
2017-08-29 23:54:03 -0300

    funcionalidad de elección de asientos y calculo de total

--- dist/js/script.js
+++ dist/js/script.js
@@ -12629,3 +12629,17 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+$(document).ready(function(){
+	$(function () {
+	  $('[data-toggle="tooltip"]').tooltip()
+	});
+});
+	var countChecked = function() {
+	  var n = $( "input:checked" ).length;
+	  $( "#monto-final" ).text( n * 4300 );
+	};
+	countChecked();
+	 
+	$( "input[type=checkbox]" ).on( "click", countChecked );
+
